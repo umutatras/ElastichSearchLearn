dfcc95b5638ef4d540f451bfa9f5122f08cbf3c2
Umut Atraş <umutatras@outlook.com>
2024-07-05 14:52:12 +0300

    Blog Projesi oluşturuldu.Els dahil edildi.

---
+++ Elasticsearch.WEB/Extensions/ElasticSearchExt.cs
@@ -0,0 +1,24 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+
+namespace Elasticsearch.WEB.Extensions
+{
+    public static class ElasticSearchExt
+    {
+        public static void AddElastic(this IServiceCollection services, IConfiguration configuration)
+        {
+            var userName = configuration.GetSection("Elastic")["Username"];
+            var password = configuration.GetSection("Elastic")["Password"];
+            var settings = new ElasticsearchClientSettings(new Uri(configuration.GetSection("Elastic")["Url"]!)).Authentication(new BasicAuthentication(userName!, password!));
+
+            var client = new ElasticsearchClient(settings);
+
+            //nest için config
+            //var pool = new SingleNodeConnectionPool(new Uri(configuration.GetSection("Elastic")["Url"]!));
+            //var settings = new ConnectionSettings(pool);
+            ////settings.BasicAuthentication(); username,password burada belirtilebilir.
+            //var client = new ElasticClient(settings);
+            services.AddSingleton(client);
+        }
+    }
+}
---
+++ Elasticsearch.WEB/Repository/BlogRepository.cs
@@ -0,0 +1,26 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elasticsearch.WEB.Models;
+
+namespace Elasticsearch.WEB.Repository
+{
+    public class BlogRepository
+    {
+        private readonly ElasticsearchClient _client;
+        private const string indexName = "blog";
+        public BlogRepository(ElasticsearchClient client)
+        {
+            _client = client;
+        }
+        public async Task<Blog?> SaveAsync(Blog newBlog)
+        {
+            newBlog.Created = DateTime.Now;
+            var response = await _client.IndexAsync(newBlog, x => x.Index(indexName).Id(Guid.NewGuid().ToString()));
+            if (!response.IsValidResponse)
+            {
+                return null;
+            }
+            newBlog.Id=response.Id;
+            return newBlog;
+        }
+    }
+}

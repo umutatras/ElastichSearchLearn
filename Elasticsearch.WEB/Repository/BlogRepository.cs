7aaff3bbb95912a2e2ab0f40b79b0269978cfdfc
Umut Atraş <umutatras@outlook.com>
2024-07-08 20:08:51 +0300

    .

--- Elasticsearch.WEB/Repository/BlogRepository.cs
+++ Elasticsearch.WEB/Repository/BlogRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
 using Elasticsearch.WEB.Models;
 
 namespace Elasticsearch.WEB.Repository
@@ -21,6 +22,57 @@ namespace Elasticsearch.WEB.Repository
             }
             newBlog.Id=response.Id;
             return newBlog;
+        }
+
+        public async Task<List<Blog>> SearchAsync(string searchText)
+        {
+
+            List<Action<QueryDescriptor<Blog>>> ListQuery = new();
+
+
+            Action<QueryDescriptor<Blog>> matchAll = (q) => q.MatchAll();
+
+            Action<QueryDescriptor<Blog>> matchContent = (q) => q.Match(m => m
+                .Field(f => f.Content)
+                .Query(searchText));
+
+
+            Action<QueryDescriptor<Blog>> titleMatchBoolPrefix = (q) => q.MatchBoolPrefix(m => m
+                .Field(f => f.Content)
+                .Query(searchText));
+
+
+            Action<QueryDescriptor<Blog>> tagTerm = (q) => q.Term(t => t.Field(f => f.Tags).Value(searchText));
+
+
+            if (string.IsNullOrEmpty(searchText))
+            {
+                ListQuery.Add(matchAll);
+            }
+
+            else
+            {
+
+                ListQuery.Add(matchContent);
+                ListQuery.Add(matchContent);
+                ListQuery.Add(tagTerm);
+            }
+
+
+            var result = await _client.SearchAsync<Blog>(s => s.Index(indexName)
+                .Size(1000).Query(q => q
+                    .Bool(b => b
+                        .Should(ListQuery.ToArray()))));
+
+            foreach (var hit in result.Hits) hit.Source.Id = hit.Id;
+            return result.Documents.ToList();
+
+
+
+
+
+
+
         }
     }
 }

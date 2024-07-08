93336f3d10ef773caa3ec114ef4000812752848f
Umut Atraş <umutatras@outlook.com>
2024-07-08 17:27:45 +0300

    blog kaydetme service yazıldı

---
+++ Elasticsearch.WEB/Services/BlogService.cs
@@ -0,0 +1,28 @@
+﻿using Elasticsearch.WEB.Models;
+using Elasticsearch.WEB.Repository;
+using Elasticsearch.WEB.ViewModel;
+
+namespace Elasticsearch.WEB.Services
+{
+    public class BlogService
+    {
+        private readonly BlogRepository _blog;
+
+        public BlogService(BlogRepository blog)
+        {
+            _blog = blog;
+        }
+        public async Task<bool> SaveAsync(BlogCreateViewModel model)
+        {
+            Blog newBlog = new Blog();
+            newBlog.UserId = Guid.NewGuid();
+            newBlog.Content= model.Content;
+            newBlog.Title = model.Title;
+            newBlog.Tags = model.Tags.ToArray();  
+            
+            var data=await _blog.SaveAsync(newBlog);
+            return data != null;
+        }
+
+    }
+}
---
+++ Elasticsearch.WEB/ViewModel/BlogCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Elasticsearch.WEB.ViewModel
+{
+    public class BlogCreateViewModel
+    {
+        [Required]
+        public string Title { get; set; } = null!;
+        [Required]
+        public string Content { get; set; } = null!;
+        [Required]
+        public List<string> Tags { get; set; } = new();
+    }
+}
